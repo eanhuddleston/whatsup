ad83d175d9632db0122492fae7a7629cdd943d73
Ean Huddleston <ean.huddleston@gmail.com>
2013-04-29 14:53:20 -0700

    Move Event db query to home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,15 +8,20 @@ class HomeController < ApplicationController
       redirect_to new_user_session_url and return
     end
 
+    @user = current_user
+    @user_loc = [@user.latitude, @user.longitude]
+
     if params[:distance]
       @distance = params[:distance]
     else
       @distance = 3
     end
 
+    @events = Event.near(@user_loc, @distance)
+
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render json: @location }
+      format.json { render json: @events }
     end
   end
 
