b8820fa7b09315ae68fffef53b36b1d60e8e6b0c
Ean Huddleston <ean.huddleston@gmail.com>
2013-04-29 11:37:50 -0700

    Add simple api key

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -3,7 +3,7 @@ module HomeHelper
     Event.near([loc[0], loc[1]], dist)
   end
 
-  def create_request(user_loc, nearby_events)
+  def create_request(user_loc, nearby_events, simple_api_key)
     events_coords = nearby_events.map {|event| "#{event.latitude},#{event.longitude}" }
     markers_string = events_coords.join("|")
     user_marker_string = "color:blue|#{user_loc[0]},#{user_loc[1]}|"
@@ -12,6 +12,7 @@ module HomeHelper
 
     search_string = "http://maps.googleapis.com/maps/api/staticmap?size=450x300" +
         "&sensor=false&zoom=12&center=#{user_loc[0]},#{user_loc[1]}" +
+        "&key=#{simple_api_key}" +
         "&markers=#{user_marker_string}" +
         "&markers=color:red|#{markers_string}"
 
